fa7ae1a736130f69a8b0c814fa719e3b15d89d58
kotaueda <k.u.okome3214@gmail.com>
2020-07-01 09:35:36 +0900

    add signin

---
+++ tutorial/app/controllers/SignupController.php
@@ -0,0 +1,31 @@
+<?php
+
+class SignupController extends Phalcon\Mvc\Controller
+{
+
+    public function indexAction()
+    {
+
+    }
+
+    public function registerAction()
+    {
+
+        $user = new Users();
+
+        // データを保存し、エラーをチェックする
+        $success = $user->save($this->request->getPost(), array('name', 'email'));
+
+        if ($success) {
+            echo "Thanks for registering!";
+        } else {
+            echo "Sorry, the following problems were generated: ";
+            foreach ($user->getMessages() as $message) {
+                echo $message->getMessage(), "<br/>";
+            }
+        }
+
+        $this->view->disable();
+    }
+
+}
---
+++ tutorial/app/models/Users.php
@@ -0,0 +1,10 @@
+<?php
+
+class Users extends Phalcon\Mvc\Model
+{
+    public $id;
+
+    public $name;
+
+    public $email;
+}
